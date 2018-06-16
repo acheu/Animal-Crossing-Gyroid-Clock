295668a0c27b023aa0519c8a2dbe2c68fc99d39f
Acheu <acheu3@gatech.edu>
2018-06-15 22:53:08 -0400

    gpio update

--- gpio_handler.py
+++ gpio_handler.py
@@ -24,8 +24,8 @@ class gpio_handler(object):
         GPIO.setup(35, GPIO.IN)  # Setup as input
         GPIO.setup(37, GPIO.OUT)  # Setup as output from pi
 
-        GPIO.add_event_detect(self.chanlist[1], GPIO.BOTH)
-        GPIO.add_event_detect(self.chanlist[3], GPIO.FALLING, callback=callback_SHTDWN)
+        GPIO.add_event_detect(self.chanlist[1], GPIO.BOTH)  
+        GPIO.add_event_detect(self.chanlist[3], GPIO.FALLING, callback=callback_SHTDWN, bouncetime=200)
 
     
 
